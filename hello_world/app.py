5f9f353a1064f11426b7441a0bbc807b49671100
Steven Senatori <steven.senatori@ge.com>
2020-10-07 17:05:20 -0700

    Working SAM with basic Chalice

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,70 @@
 import json
+from chalice import Chalice
 
-# import requests
+app = Chalice(app_name='chalice-sam-jenkins')
 
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
+@app.route('/')
+def index():
+    return {'hello': 'world'}
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
 
-    context: object, required
-        Lambda Context runtime methods and attributes
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
 
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello claurice",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+# def lambda_handler(event, context):
+#     """Sample pure Lambda function
+#
+#     Parameters
+#     ----------
+#     event: dict, required
+#         API Gateway Lambda Proxy Input Format
+#
+#         Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+#
+#     context: object, required
+#         Lambda Context runtime methods and attributes
+#
+#         Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+#
+#     Returns
+#     ------
+#     API Gateway Lambda Proxy Output Format: dict
+#
+#         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+#     """
+#
+#     # try:
+#     #     ip = requests.get("http://checkip.amazonaws.com/")
+#     # except requests.RequestException as e:
+#     #     # Send some context about this error to Lambda Logs
+#     #     print(e)
+#
+#     #     raise e
+#
+#     return {
+#         "statusCode": 200,
+#         "body": json.dumps({
+#             "message": "hello claurice",
+#             # "location": ip.text.replace("\n", "")
+#         }),
+#     }
