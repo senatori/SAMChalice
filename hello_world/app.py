df720f5f4629babaf456e4f9da4249eef3b545b3
Steven Senatori <steven.senatori@ge.com>
2020-10-07 17:48:26 -0700

    Updating routes

--- hello_world/app.py
+++ hello_world/app.py
@@ -3,10 +3,30 @@ from chalice import Chalice
 
 app = Chalice(app_name='chalice-sam-jenkins')
 
+CITIES_TO_STATE = {
+    'seattle': 'WA',
+    'portland': 'OR',
+}
 
 @app.route('/')
 def index():
     return {'hello': 'world'}
+
+@app.route('/cities/{city}')
+def state_of_city(city):
+    return {'state': CITIES_TO_STATE[city]}
+
+@app.route('/hello/{name}')
+def hello_name(name):
+   # '/hello/james' -> {"hello": "james"}
+   return {'hello': name}
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    # This is the JSON body the user sent in their POST request.
+    user_as_json = app.current_request.json_body
+    # We'll echo the json body back to the user in a 'user' key.
+    return {'user': user_as_json}
 
 
 # The view function above will return {"hello": "world"}
